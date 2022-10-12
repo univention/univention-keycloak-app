dc97c7476e8b319a2a8bd452874fd80e22d94a3b
Juan Pedro Torres <torres-munoz.extern@univention.de>
2022-10-12 16:05:04 +0200

    Issue #10: ship keycloak.conf

--- update-appcenter-test.sh
+++ update-appcenter-test.sh
@@ -44,7 +44,8 @@ sed -i -e "/%ARCHIVE_CONTENT%/r files/tmp_base64" -e "/%ARCHIVE_CONTENT%/d" app/
 
 sed -i -e "/%KEYCLOAK-TEMPLATE-APACHE%/r files/univention-keycloak.conf" -e "/%KEYCLOAK-TEMPLATE-APACHE%/d" app/preinst
 sed -i -e "/%KEYCLOAK-INFO-APACHE%/r files/univention-keycloak.info" -e "/%KEYCLOAK-INFO-APACHE%/d" app/preinst
-sed -i -e "/%KEYCLOAK-TEMPLATE-CONF%/r files/keycloak.info" -e "/%KEYCLOAK-TEMPLATE-CONF%/d" app/preinst
+base64 files/keycloak.conf >> files/tmp_kconf_b64
+sed -i -e "/%KEYCLOAK-TEMPLATE-CONF%/r files/tmp_kconf_b64" -e "/%KEYCLOAK-TEMPLATE-CONF%/d" app/preinst
 
 ## Now we can upload the files for the app to the provider-portal:
 ## The order of the arguments doesn't matter, the univention-appcenter-control script recongnizes the filenames and file extensions.
@@ -54,4 +55,4 @@ selfservice upload "$APP_VERSION" app/compose app/settings app/preinst app/confi
 # selfservice upload "$APP_VERSION" app/compose app/settings app/preinst app/configure_host app/inst app/uinst app/env app/test app/setup README_*
 
 ## And finally they clean the working directory after upload
-rm -f app/preinst files/tmp_base64 files/tmp_uk_b64
+rm -f app/preinst files/tmp_base64 files/tmp_uk_b64 files/tmp_kconf_b64
