1e63a8f01c7ac4c538264d1f529716b45ede65c7
Felix Botner <botner@univention.de>
2023-04-20 16:01:28 +0200

    Issue #62: added local mode to update-appcenter-test.sh

--- update-appcenter-test.sh
+++ update-appcenter-test.sh
@@ -4,12 +4,23 @@
 #
 
 set -e
-set -x
 
 UCS_VERSION="5.0"
 APP_ID="keycloak"
 VERSION="$(sed -n 's/^Version\s*=\s*//p' app/ini)"
 APP_VERSION="${UCS_VERSION}/${APP_ID}=${VERSION}"
+FILES_TO_COPY=(
+	"app/compose"
+	"app/settings"
+	"app/preinst"
+	"app/configure_host"
+	"app/inst"
+#	"app/env"
+	"app/test"
+	"app/uinst"
+	"app/README_UPDATE_DE"
+	"app/README_UPDATE_EN"
+)
 
 selfservice () {
 	local uri="https://provider-portal.software-univention.de/appcenter-selfservice/univention-appcenter-control"
@@ -17,12 +28,12 @@ selfservice () {
 	shift
 
 	USERNAME="$USER"
-	[ -e "$HOME/.univention-appcenter-user" ] && USERNAME="$(< $HOME/.univention-appcenter-user)"
+	[ -e "$HOME/.univention-appcenter-user" ] && USERNAME="$(< "$HOME"/.univention-appcenter-user)"
 
-	PWDFILE="~/.selfservicepwd"
+	PWDFILE="$HOME/.selfservicepwd"
 	[ -e "$HOME/.univention-appcenter-pwd" ] && PWDFILE="$HOME/.univention-appcenter-pwd"
 
-	curl -sSfL "$uri" | python3 - "$first" --username=${USERNAME} --pwdfile=${PWDFILE} "$@"
+	curl -sSfL "$uri" | python3 - "$first" --username="${USERNAME}" --pwdfile="${PWDFILE}" "$@"
 }
 
 die () {
@@ -30,7 +41,61 @@ die () {
 	exit 0
 }
 
+get_cache_file () {
+	local file="$1"
+	python3 <<-EOF
+		from univention.appcenter.app_cache import Apps
+		apps_cache = Apps()
+		candidate = apps_cache.find("keycloak", latest=True)
+		print(candidate.get_cache_file("$file"))
+	EOF
+}
+
+usage () {
+	echo "${0##*/} [options]"
+	echo ""
+	echo "copy keycloak app files to test appcenter or to local cache (latest keycloak version)"
+	echo ""
+	echo "Options:"
+	echo "  -h, --help      show this help message and exit"
+	echo "  -l, --local    copy files to local cache (latest keycloak version)"
+	echo "  -d, --dryn-run  dry-run, just print don't copy"
+}
+
+
 [ "$IGN_GIT" != "true" ] && test -n "$(git status -s)" && die "Changes in repo, do not upload app! (to override: IGN_GIT=true)"
+
+# read arguments
+opts=$(getopt \
+	--longoptions "help,local,dry-run" \
+	--name "$(basename "$0")" \
+	--options "hld" \
+	-- "$@"
+) || die "see -h|--help"
+eval set -- "$opts"
+local_copy=false
+dry_run=false
+while true
+do
+	case "$1" in
+		-h|--help)
+			usage
+			exit 0
+			;;
+		-l|--local)
+			local_copy=true
+			shift
+			;;
+		-d|--dry-run)
+			dry_run=true
+			shift
+			;;
+		--)
+			shift
+			break
+			;;
+	esac
+done
 
 ## Here we could e.g. generate the scripts from templates and adjust things, see e.g.:
 ## * https://git.knut.univention.de/univention/components/dashboard/admin-dashboard
@@ -60,19 +125,23 @@ sed -i -e "/%KEYCLOAK-LOGIN-TRANSLATION-DE%/r files/messages_de.properties" -e "
 sed -i -e "/%KEYCLOAK-LOGIN-TRANSLATION-EN%/r files/messages_en.properties" -e "/%KEYCLOAK-LOGIN-TRANSLATION-EN%/d" app/preinst
 sed -i -e "/%KEYCLOAK-LOGIN-TRANSLATION-INFO-DE%/r files/messages_de.properties.info" -e "/KEYCLOAK-LOGIN-TRANSLATION-INFO-DE%/d" app/preinst
 sed -i -e "/%KEYCLOAK-LOGIN-TRANSLATION-INFO-EN%/r files/messages_en.properties.info" -e "/%KEYCLOAK-LOGIN-TRANSLATION-INFO-EN%/d" app/preinst
-## Now we can upload the files for the app to the provider-portal:
-## The order of the arguments doesn't matter, the univention-appcenter-control script recongnizes the filenames and file extensions.
-selfservice upload "$APP_VERSION" \
-    app/compose \
-    app/settings \
-    app/preinst \
-    app/configure_host \
-    app/inst \
-    app/env \
-    app/test \
-    app/uinst \
-    app/README_UPDATE_DE \
-    app/README_UPDATE_EN
+
+if "$local_copy"; then
+	for file in "${FILES_TO_COPY[@]}"; do
+		cache_file="$(get_cache_file "$(basename "$file")")"
+		echo "$file -> $cache_file"
+		if ! "$dry_run"; then
+			cp "$file" "$cache_file"
+		fi
+	done
+else
+	### Now we can upload the files for the app to the provider-portal:
+	### The order of the arguments doesn't matter, the univention-appcenter-control script recongnizes the filenames and file extensions.
+	echo "selfservice upload $APP_VERSION ${FILES_TO_COPY[*]}"
+	if ! "$dry_run"; then
+		selfservice upload "$APP_VERSION" "${FILES_TO_COPY[@]}"
+	fi
+fi
 
 ## There are more "magic" files that can be uploaded for specific purposes:
 # selfservice upload "$APP_VERSION" app/compose app/settings app/preinst app/configure_host app/inst app/uinst app/env app/test app/setup README_*
