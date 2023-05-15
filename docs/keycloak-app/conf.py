2eb8e4c359c702fa3a450a03a9c00ca69fb34089
Nico Gulden <gulden@univention.de>
2023-05-15 12:01:56 +0200

    Issue #68: docs: Remove unused numfig_format configuration
    The `numfig_format` custom configuration is not used. I removed it to
    use the Sphinx default.

--- docs/keycloak-app/conf.py
+++ docs/keycloak-app/conf.py
@@ -120,12 +120,6 @@ html_static_path = []  # value is usally ['_static']
 html_last_updated_fmt = "%a, %d. %b %Y at %H:%m (UTC%z)"
 
 numfig = True
-numfig_format = {
-    "figure": _("Figure %s"),
-    "table": _("Table %s"),
-    "code-block": _("Listing %s"),
-    "section": _("Section %s"),
-}
 
 if "spelling" in sys.argv:
     spelling_lang = "en"
