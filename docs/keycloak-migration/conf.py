ad38409bee3ea3016142ddcffa999728ed181890
Nico Gulden <gulden@univention.de>
2023-08-18 11:49:12 +0200

    Issue #155: doc(migration): Activate website tracking
    Activating website tracking adds code to every HTML page that tracks
    visitor activity without setting cookies to the Matomo instance operated
    by Univention at https://www.piwik.univention.de/

--- docs/keycloak-migration/conf.py
+++ docs/keycloak-migration/conf.py
@@ -84,6 +84,7 @@ html_theme_options = {
     "typesense_search": True,
     "typesense_document": doc_base,
     "typesense_document_version": "latest",
+    "univention_matomo_tracking": True,
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
