7f23041674ec8d31b599fdbbdf7a8c8ac6698fff
Julia Bremer <bremer@univention.de>
2023-08-21 09:46:17 +0200

    Issue #148: Add login custom.css as well

--- files/themes/UCS/login/resources/univention.js
+++ files/themes/UCS/login/resources/univention.js
@@ -1,3 +1,4 @@
 document.write('<link rel="stylesheet" type="text/css" href="/univention/theme.css">');
+document.write('<link rel="stylesheet" type="text/css" href="/univention/login/css/custom.css">');
 document.write('<link rel="icon" href="/favicon.ico">');
 
