06cc099c24a0c309152c53be6a77d2bd486e2d93
Julia Bremer <bremer@univention.de>
2023-12-11 15:55:57 +0100

    Issue #178: Fix documentation pipeline

--- docs/keycloak-app/conf.py
+++ docs/keycloak-app/conf.py
@@ -45,8 +45,8 @@ def read_version_from_ci() -> str:
     with open("../../.gitlab-ci.yml") as f:
         ci = yaml.safe_load(f)
         return ci.get(
-            "variables", {"DOC_TARGET_VERSION": "18.0.0"},
-        ).get("DOC_TARGET_VERSION")
+            "variables", {"KEYCLOAK_VERSION": "18.0.0"},
+        ).get("KEYCLOAK_VERSION")
 
 
 release = read_version_from_ci()
