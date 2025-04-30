a61a627f7562318add4990019f42267639d49dbb
Nico Gulden <gulden@univention.de>
2025-04-30 16:21:21 +0200

    fix(docs): Update UCS Manual reference to UCS 5.2
    The document used the deprecated reference to UCS 5.0. I fixed and
    updated it to 5.2.
    
    Issue univention/dev/docs/nubus-docs#91

--- docs/keycloak-app/conf.py
+++ docs/keycloak-app/conf.py
@@ -144,7 +144,7 @@ rst_epilog = """
 """
 
 intersphinx_mapping = {
-    "uv-manual": ("https://docs.software-univention.de/manual/5.0/en", None),
+    "uv-manual": ("https://docs.software-univention.de/manual/5.2/en", None),
     "uv-nubus-kubernetes-operation": ("https://docs.software-univention.de/nubus-kubernetes-operation/1.x/en/", None),
 }
 
